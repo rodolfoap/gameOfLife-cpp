5524053377dcb1ac43e62111f8c1b6d60e8944c3
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-05-11 18:34:47 +0200

    Simple text
    Change-Id: Ieb6500188238c73ed06d353f325cec402634951d

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -2,25 +2,22 @@
 #include <fstream>
 #include <string.h>
 #include <unistd.h>
-#include <ncurses.h>
-#include "functions.cpp"
 #define B(p, q)(b[y+p][x+q]=='#')
-
 int main(int n, char**v) {
 	int col=0, row=0;
 	char b[128][256];
 	std::ifstream f(v[1]);
 	while(f>>b[row++]);
 	col=strlen(b[0]);
-	initncurses();
-
 	while(1) {
+		std::cout<<"\033[2J\033[1;1H"; // Clear Screen
+
 		// Calculate neighbors number and print actual state
 		int i[128][256]{0};
 		for(int y=0; y<row; ++y) { for(int x=0; x<col; ++x) {
 			i[y][x]=B(-1,-1)+B(-1,0)+B(-1,1)+B(0,-1)+B(0,1)+B(1,-1)+B(1,0)+B(1,1);
-			dot(y, x, b[y][x]=='#'?1:0);
-		}} refresh();
+			std::cerr<<b[y][x];
+		} std::cerr<<std::endl; } std::cerr<<std::endl;
 
 		// Apply rules over neighbors number
 		for(int y=0; y<row; ++y) { for(int x=0; x<col; ++x) {
