f8dbf9a2a5992d85a493f985ae4540e6fae48206
RodolfoAP <rodolfoap@gmail.com>
2020-05-12 22:59:09 +0200

    Changes 2020/05/12-22:59:09
    Change-Id: Icaa74beea6ef74788075204efb1345ecb5e2e694

--- plaintext.cpp
+++ plaintext.cpp
@@ -3,9 +3,9 @@ void initscreen() {
 	std::cerr<<std::endl;
 }
 void updatescreen(){
-	std::cerr<<std::endl;
+	std::cout<<"\033[2J\033[1;1H"; // Clear Screen
 }
 void setdot(int y, int x, int v){
-	if(x==0) std::cerr<<std::endl;
+	if(x==0&&y!=0) std::cerr<<std::endl;
 	std::cerr<<(v?'#':'.');  
 }
